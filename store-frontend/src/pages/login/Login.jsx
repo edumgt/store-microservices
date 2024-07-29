8d7ee82eee5f5de8015c2b42cab8231d5bb6a0c6
praveenukkoji <praveenukkoji@gmail.com>
2024-07-29 20:14:29 +0530

    axios added for login

--- store-frontend/src/pages/login/Login.jsx
+++ store-frontend/src/pages/login/Login.jsx
@@ -1,8 +1,29 @@
+import axios from "axios";
+
 /* eslint-disable react/no-unescaped-entities */
 export default function Login() {
   const loggedIn = "true" === localStorage.getItem("loggedIn");
   if (loggedIn) {
     window.location.href = "/home";
+  }
+
+  function login() {
+    const username = document.getElementById("inputUsername").value;
+    const password = document.getElementById("inputPassword").value;
+
+    const apiGatewayPort = "8000";
+
+    const URL = "http://localhost:" + apiGatewayPort + "/api/v1/users/login";
+
+    const body = {
+      username: username,
+      password: password,
+    };
+
+    axios
+      .post(URL, body)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
   }
 
   return (
@@ -16,13 +37,13 @@ export default function Login() {
           className="mt-4"
         >
           <div className="mb-3">
-            <label className="form-label">Email address</label>
+            <label className="form-label">Username</label>
             <input
-              type="email"
+              type="text"
               className="form-control"
-              id="inputEmail"
-              aria-describedby="emailHelp"
-              placeholder="example@domain.com"
+              id="inputUsername"
+              aria-describedby="usernameHelp"
+              placeholder="username"
             />
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
@@ -38,7 +59,9 @@ export default function Login() {
             />
           </div>
           <div className="text-center mt-4">
-            <button className="w-25 btn btn-primary">Log In</button>
+            <button className="w-25 btn btn-primary" onClick={() => login()}>
+              Log In
+            </button>
           </div>
         </div>
       ) : null}
