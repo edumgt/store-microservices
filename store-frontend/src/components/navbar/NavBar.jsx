f942013e77ce5dbcb48a5d52236615d1e1b771bf
praveenukkoji <praveenukkoji@gmail.com>
2024-08-12 22:20:27 +0530

    navbar position fixed at top

--- store-frontend/src/components/navbar/NavBar.jsx
+++ store-frontend/src/components/navbar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             {title}
