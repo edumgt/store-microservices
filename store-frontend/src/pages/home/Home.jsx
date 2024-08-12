1846790b2e2f2506ee201a8a376929854a6690ed
praveenukkoji <praveenukkoji@gmail.com>
2024-08-12 22:19:53 +0530

    alert component removed

--- store-frontend/src/pages/home/Home.jsx
+++ store-frontend/src/pages/home/Home.jsx
@@ -1,18 +1,14 @@
-import Alert from "../../components/alert/Alert";
 import Pagination from "../../components/pagination/Pagination";
 import SearchBar from "../../components/searchbar/SearchBar";
 import Products from "../products/Products";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="home-container m-4">
-        <Alert />
-        <SearchBar />
-        <Products />
-        <div className="d-flex justify-content-center mt-4">
-          <Pagination />
-        </div>
+    <div className="home-container p-4">
+      <SearchBar />
+      <Products />
+      <div className="d-flex justify-content-center mt-4">
+        <Pagination />
       </div>
     </div>
   );
