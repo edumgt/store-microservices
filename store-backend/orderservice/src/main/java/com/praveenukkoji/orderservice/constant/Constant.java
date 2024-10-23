8d5b8af9acae87bf5fa99a02dfca6f8e3347897b
praveenukkoji <praveenukkoji@gmail.com>
2024-10-23 20:03:44 +0530

    notification, order, product, user all services docker config

--- store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/constant/Constant.java
+++ store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/constant/Constant.java
@@ -1,14 +1,16 @@
 package com.praveenukkoji.orderservice.constant;
 
 public final class Constant {
-    
-    public static final String API_VERSION = "v1";
+
+    private static final String PRODUCT_HOSTNAME = "productservice";
+
+    private static final String API_VERSION = "v1";
 
     public static final String API_GATEWAY_PRODUCT_SERVICE_URL =
-            "http://localhost:8222/api/" + API_VERSION + "/products";
+            "http://" + PRODUCT_HOSTNAME + ":8222/api/" + API_VERSION + "/products";
 
     public static final String PRODUCT_SERVICE_URL =
-            "http://localhost:8001/api/" + API_VERSION + "/products";
+            "http://" + PRODUCT_HOSTNAME + ":8001/api/" + API_VERSION + "/products";
 
     // Private constructor to prevent instantiation
     private Constant() {
