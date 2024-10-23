a016a39aa4a103ee20ec0ee591e7ea65315c0c14
praveenukkoji <praveenukkoji@gmail.com>
2024-10-24 00:27:31 +0530

    product feign client url updated

--- store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/constant/Constant.java
+++ store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/constant/Constant.java
@@ -2,7 +2,7 @@ package com.praveenukkoji.orderservice.constant;
 
 public final class Constant {
 
-    public static final String PRODUCT_SERVICE_URL = "http://product-service:8001/api/v1/products";
+    public static final String PRODUCT_SERVICE_URL = "http://productservice:8001/api/v1/products";
 
     private Constant() {
     }
--- store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/external/product/feignClient/ProductClient.java
+++ store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/external/product/feignClient/ProductClient.java
@@ -1,6 +1,5 @@
 package com.praveenukkoji.orderservice.external.product.feignClient;
 
-import com.praveenukkoji.orderservice.constant.Constant;
 import com.praveenukkoji.orderservice.external.product.dto.request.ProductDetailRequest;
 import com.praveenukkoji.orderservice.external.product.dto.response.ProductDetailResponse;
 import feign.Headers;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(value = "product-client", url = Constant.PRODUCT_SERVICE_URL)
+@FeignClient(value = "product-client", url = "http://productservice:8001/api/v1/products")
 public interface ProductClient {
 
     // get product detail
