8b4d2940961f4084f1d5acfd21601b78946f50c2
praveenukkoji <praveenukkoji@gmail.com>
2024-10-27 20:21:32 +0530

    constants file deleted

--- store-backend/orderservice/src/main/java/com/praveenukkoji/orderservice/constant/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.praveenukkoji.orderservice.constant;
-
-public final class Constant {
-
-    public static final String PRODUCT_SERVICE_URL = "http://productservice:8001/api/v1/products";
-
-    private Constant() {
-    }
-}
