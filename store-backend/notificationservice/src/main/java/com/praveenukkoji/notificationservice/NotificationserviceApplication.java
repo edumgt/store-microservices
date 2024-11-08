a7b057e4e4b800d427b13c658acc33376df2d7af
praveenukkoji <praveenukkoji@gmail.com>
2024-11-09 00:10:04 +0530

    email service added in notification service

--- store-backend/notificationservice/src/main/java/com/praveenukkoji/notificationservice/NotificationserviceApplication.java
+++ store-backend/notificationservice/src/main/java/com/praveenukkoji/notificationservice/NotificationserviceApplication.java
@@ -1,16 +1,25 @@
 package com.praveenukkoji.notificationservice;
 
 import com.praveenukkoji.notificationservice.event.OrderEvent;
+import com.praveenukkoji.notificationservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.KafkaListener;
 
 @Slf4j
+@RequiredArgsConstructor
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NotificationserviceApplication {
+
+    private final EmailService emailService;
+
+    @Value("${spring.mail.to}")
+    private String to;
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationserviceApplication.class, args);
@@ -21,6 +30,8 @@ public class NotificationserviceApplication {
         // send email notification
         log.info("ORDER NOTIFICATION ---- received notification title: {}, message: {}",
                 orderEvent.getTitle(), orderEvent.getMessage());
+
+        emailService.sendEmail(to, orderEvent.getTitle(), orderEvent.getMessage());
     }
 
 }
---
+++ store-backend/notificationservice/src/main/java/com/praveenukkoji/notificationservice/service/EmailService.java
@@ -0,0 +1,37 @@
+package com.praveenukkoji.notificationservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Slf4j
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendEmail(String to, String subject, String body) {
+        log.info("Sending email to {}", to);
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            message.setFrom(from);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
