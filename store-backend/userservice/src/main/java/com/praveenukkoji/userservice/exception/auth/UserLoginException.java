31a43b49fed0590bf636eba21c1f6793815e3f5c
praveenukkoji <praveenukkoji@gmail.com>
2024-11-12 02:46:03 +0530

    user-service application.yaml done

--- store-backend/userservice/src/main/java/com/praveenukkoji/userservice/exception/auth/UserLoginException.java
+++
@@ -1,7 +0,0 @@
-package com.praveenukkoji.userservice.exception.auth;
-
-public class UserLoginException extends Exception {
-    public UserLoginException(String message) {
-        super(message);
-    }
-}
